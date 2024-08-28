f3e2d4964375e83f3a8f768756ef3f4d8407b356
HenryMalik <henrymalik03@gmail.com>
2024-08-28 18:58:06 +0700

    a a

--- belajar_react/src/home.tsx
+++ belajar_react/src/home.tsx
@@ -30,23 +30,98 @@ const userData = [
                     'region': 'asia'
                     },
 ]
+// form : sword, bow, heavy, staff/wand, catalyst, shield
+// type : physical, magic, utility
+// origin : human, elf, dwarf, demigod/archmage
+// material : metal, wood, crystal, monster parts
+const FantasyWeapons = [
+    {
+      'name': 'Broadsword',
+      'form': 'sword',
+      'type': 'physical',
+      'damage': 12,
+      'color': 'iron grey',
+      'origin': 'human',
+      'material': 'metal'
+    },
+    {
+        'name': 'Aegis',
+        'form': 'shield',
+        'type': 'utility',
+        'damage': 0,
+        'color': 'oak',
+        'origin': 'human',
+        'material': 'metal'
+      },
+      {
+        'name': 'Recurve',
+        'form': 'bow',
+        'type': 'physical',
+        'damage': 15,
+        'color': 'oak',
+        'origin': 'elf',
+        'material': 'wood'
+      },
+      {
+        'name': 'Claymore',
+        'form': 'heavy',
+        'type': 'physical',
+        'damage': 23,
+        'color': 'iron grey',
+        'origin': 'dwarf',
+        'material': 'metal'
+      },
+      {
+        'name': 'Scepter',
+        'form': 'staff',
+        'type': 'magic',
+        'damage': 16,
+        'color': 'red',
+        'origin': 'archmage',
+        'material': 'crystal'
+      },
+      {
+        'name': 'Smchorgs Core',
+        'form': 'catalyst',
+        'type': 'magic',
+        'damage': 96,
+        'color': 'purple',
+        'origin': 'demigod',
+        'material': 'monster parts'
+      },
+]
 
 const filteredData = userData.filter(item => item.age % 2 !== 0)
 console.log(filteredData, '=== filtered data');
+const starterweapon = FantasyWeapons.filter(item => item.damage <= 25 )
+console.log(starterweapon, '=== starter weapon');
 const Home = () =>{
     return (
         <div>
+            <h1>home screen</h1>
             {userData.map((item, index) => {
                 let ItemGender = item.age
                 return (
                     <span style={{
-                        color: ItemGender % 2 == 0 && '#db1413' 
+                        color: ItemGender % 2 == 0 && '#db1413'  
                     }}> {item.name}</span>
                 )
             })}
-            <h1>home screen</h1>
         </div>
     )
+    const Home2 = () => {
+    return (
+            <div>
+                <p>Starter Weapon</p>
+                {FantasyWeapons.map((item, index) => {
+                    let ItemDamage = item.damage
+                    return (
+                        <span> {item.damage} </span>
+                    )
+                })}
+            </div>
+        )
+    }
 }
 
 
