0231acb6545c0ca8d407091ffe10c77ca8dd5226
HenryMalik <henrymalik03@gmail.com>
2024-10-01 15:38:17 +0700

    tugas website cooking template

--- belajar_react/src/route/root/tugas/index.tsx
+++ belajar_react/src/route/root/tugas/index.tsx
@@ -11,14 +11,18 @@ return (
 <div className={styles.maincontent}>
 <div className={styles.testbox}>
         <div className={styles.box}>
-            <h3>Hi!</h3>
+            <h3>Logo</h3>
             </div>
         <div className={styles.box}>
-        <h3>Hi!</h3>
+        <h3>menu1</h3>
+        <h3>menu2</h3>
+        <h3>menu3</h3>
+        <h3>menu4</h3>
         </div>
         <div className={styles.box}>
         <h3>Hi!</h3>
         </div>
+        <div className={styles.carousel}></div>
 </div>
 </div>
 <div className={styles.sidemenu}>
