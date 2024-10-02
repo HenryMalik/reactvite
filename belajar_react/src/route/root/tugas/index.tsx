d96bc26d6ee2641ca5b54f2d2335db57631e7c5a
HenryMalik <henrymalik03@gmail.com>
2024-10-02 19:58:48 +0700

    tugas website cooking template

--- belajar_react/src/route/root/tugas/index.tsx
+++ belajar_react/src/route/root/tugas/index.tsx
@@ -14,15 +14,43 @@ return (
             <h3>Logo</h3>
             </div>
         <div className={styles.box}>
-        <h3>menu1</h3>
-        <h3>menu2</h3>
-        <h3>menu3</h3>
-        <h3>menu4</h3>
+    <a href="">menu1</a>
+    <a href="">menu2</a>
+    <a href="">menu3</a>
+    <a href="">menu4</a>
         </div>
         <div className={styles.box}>
-        <h3>Hi!</h3>
+        <h3>Social Media</h3>
         </div>
         <div className={styles.carousel}></div>
+        <div className={styles.catalog}>
+        <h2>Categories</h2>
+        <button>View All Category</button>
+            <div className={styles.category}>
+                <p>Image Placeholder</p>
+                <p>Category 1</p>
+            </div>
+            <div className={styles.category}>
+                <p>Image Placeholder</p>
+                <p>Category 2</p>
+            </div>
+            <div className={styles.category}>
+                <p>Image Placeholder</p>
+                <p>Category 3</p>
+            </div>
+            <div className={styles.category}>
+                <p>Image Placeholder</p>
+                <p>Category 4</p>
+            </div>
+            <div className={styles.category}>
+                <p>Image Placeholder</p>
+                <p>Category 5</p>
+            </div>
+            <div className={styles.category}>
+                <p>Image Placeholder</p>
+                <p>Category 6</p>
+            </div>
+        </div>
 </div>
 </div>
 <div className={styles.sidemenu}>
